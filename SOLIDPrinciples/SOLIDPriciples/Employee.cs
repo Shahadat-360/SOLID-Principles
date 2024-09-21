3a863dfcdb79a8b68d5a2d6f097169ad7d539432
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-21 21:53:30 +0600

    Single Responsibility Principle

---
+++ SOLIDPrinciples/SOLIDPriciples/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPriciples
+{
+    public class Employee
+    {
+        public double CalculatePay(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(Employee employee) 
+        {
+            try
+            {
+                //Code for saving 
+            }
+            catch (Exception ex)
+            {
+                Logger logger = new Logger();
+                logger.LogError(ex.Message);
+            }
+        }
+
+        public void ReportHours(Employee employee) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

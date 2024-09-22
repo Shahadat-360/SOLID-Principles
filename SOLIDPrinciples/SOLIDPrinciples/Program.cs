eb3f6ae7e8f9ad7d4fb23f0cdb7a1ec38e9ada6b
MD. Shahadat Hossain Bhuian <1337.Shahadat@gmail.com>
2024-09-22 17:21:06 +0600

    Open For Extension And Close For Modifications Of SOLID Principles

--- SOLIDPrinciples/SOLIDPrinciples/Program.cs
+++ SOLIDPrinciples/SOLIDPrinciples/Program.cs
@@ -4,5 +4,5 @@ using static SOLIDPrinciples.Employee;
 Employee employee = new Employee() { EmployeeType = empType.FullTime.ToString(), TotalHoursWorked = 10 };
 Employee employee2 = new Employee() { EmployeeType = empType.HalfTime.ToString(), TotalHoursWorked = 10 };
 
-EmployeeFinances employeeFinances = new EmployeeFinanceForPTE();
-Console.WriteLine(employeeFinances.CalculatePay(employee2));
+EmployeeFinances employeeFinances = new EmployeeFinanceForFTE();
+Console.WriteLine(employeeFinances.CalculatePay(employee));
